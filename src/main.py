2dbb5a752189facc55ad411dd87ca7309807c0bb
Lucas <lucas.lucantas38@gmail.com>
2019-03-29 18:32:24 -0300

    Making crop based on frame dimension

--- src/main.py
+++ src/main.py
@@ -20,8 +20,27 @@ def is_image(file):
 
     return False
 
+def get_sheet_path(args):
+    try:
+        return args[args.index("-c") + 1]
+    except ValueError:
+        return ""
+
+def get_available_frames(args):
+    try:
+        return args.index("-f") + 1
+    except ValueError:
+        return 0
+        
+def get_frame_dimension(args):
+    try:
+        return args.index("-d") + 1
+    except ValueError:
+        return 0
+
+
 def crop_frames(file):
-    # run identify to see the image size]
+    # run identify to see the image size
     output = os.popen("identify -format '%[fx:w]x%[fx:h]' " + file).read()
     # find the width
     width = int(output.split("x")[0])
@@ -34,52 +53,81 @@ def crop_frames(file):
     max_frames = (height * width)/(square_side*square_side) 
     horizontal_frames = int(width/square_side)
     vertical_frames = int(height/square_side)
-    # loop over the frames
+
+    batch_crop(file, vertical_frames, horizontal_frames, square_side, square_side)
+    
+  
+def crop_frames_with_dimension(file, dimension):
+    # run identify to see the image size
+    output = os.popen("identify -format '%[fx:w]x%[fx:h]' " + file).read()
+    # find the width
+    width = int(output.split("x")[0])
+    #find the height
+    height = int(output.split("x")[1])
+    # get the frame width from the user's input
+    frame_width = int(dimension.split("x")[0])
+    # get the frame height from the user's input
+    frame_height = int(dimension.split("x")[1])
+
+    horizontal_frames = int(width/frame_width)
+    vertical_frames = int(height/frame_height)
+
+    max_frames = horizontal_frames * vertical_frames
+    print(width, height, frame_width, frame_height)
+    print(max_frames, horizontal_frames, vertical_frames)
+
+    batch_crop(file, vertical_frames, horizontal_frames, frame_width, frame_height)
+
+def crop_frames_with_max_frames(file, frames):
+    pass
+
+def batch_crop(file, vertical_frames, horizontal_frames, frame_width, frame_height):
+    # Create the file if it doesn't exists
     try:
         os.mkdir(os.path.join(Path(file).parent, file.split(".")[0]) )
     except:
         print("dir exists")
-
+    # loop over the frames
     for i in range(int(vertical_frames)):
         for j in range(int(horizontal_frames)):
             file_name = str(i) + "_" + str(j)
             destiny = os.path.abspath(os.path.join(Path(file).parent, file.split(".")[0], file_name))
-            h_offset = square_side * j
-            w_offset = square_side * i
-            print("convert {0} -crop {1}x{1}+{2}+{3} {4}.png".format(
+            h_offset = frame_height * j
+            w_offset = frame_width * i
+            print("Executing: convert {0} -crop {1}x{2}+{3}+{4} {5}.png".format(
                     file,
-                    square_side,
+                    frame_width,
+                    frame_height,
                     h_offset,
                     w_offset,
                     destiny
                 ))
             os.popen(
-                "convert {0} -crop {1}x{1}+{2}+{3} {4}.png".format(
+                "convert {0} -crop {1}x{2}+{3}+{4} {5}.png".format(
                     file,
-                    square_side,
+                    frame_width,
+                    frame_height,
                     h_offset,
                     w_offset,
                     destiny
                 )
                 )
-  
 
-  
 
-def find_square_by_frame_dimension(width, height):
-    size = math.max(width, height)
-    columns = math.floor(width/size)
-    rows = math.floor(height/size)
-     
-
-    
 args = sys.argv
-sheet_path = args[args.index("-c") + 1]
+sheet_path = get_sheet_path(args)
+frames_available_index = get_available_frames(args)
+frames_dimension_index = get_frame_dimension(args)
 
 if (is_image(sheet_path)):
-    print("seems to be an image")
-    crop_frames(sheet_path)
-
+    if (frames_available_index > 0):
+        crop_frames_with_max_frames(sheet_path, args[frames_available_index])
+    elif (frames_dimension_index > 0):
+        crop_frames_with_dimension(sheet_path, args[frames_dimension_index])
+    else:
+        crop_frames(sheet_path)
+else:
+    print("invalid image")
 
 
 
